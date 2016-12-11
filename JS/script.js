1ab5a520be5dabf162c87d44650f0debea14de4e
andreev09 <teman77@gmail.com>
2016-12-11 19:29:15 +0300

    обновление времени по клику на лого

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,19 @@
-for (var i = 0; i < 3; i++) {
-  document.write(i);
-  for (var i = 0; i < 3; i++) {
-    document.write(i);
-    for (var i = 0; i < 3; i++) {
-      document.write(i);
-    }
-  }
+// for (var i = 0; i < 3; i++) {
+//   document.write(i);
+//   for (var i = 0; i < 3; i++) {
+//     document.write(i);
+//     for (var i = 0; i < 3; i++) {
+//       document.write(i);
+//     }
+//   }
+// // }
+// var button = document.queryselector('.clickme');
+var $button = $('.clickme');
+
+$button.click(function() { //при клике на элемент баттон проиходит функция
+
+}
+
+function sayYo() {
+
 }
--- Supreme/script.js
+++ Supreme/script.js
@@ -18,4 +18,27 @@
 // var date = document.queryselector('.date');
 // //
 
-var date = $('.date');
+var $logo = $('.logo');
+var $date = $('.date');
+
+function getDate() {
+  var realDate = new Date(); //текущая дата
+  //2 получаем дату
+
+  var day = realDate.getDate();
+  var month = realDate.getMonth() + 1;
+  var year = realDate.getFullYear();
+
+  var hour = ('0' + realDate.getHours()).slice(-2);
+  var minutes = ('0' + realDate.getMinutes()).slice(-2);
+  var seconds = ('0' + realDate.getSeconds()).slice(-2);
+
+  var dateHtml = day + '/' + month + '/' + year + ' ' + hour + ':' + minutes + ':' + seconds + ' MSC';
+
+  $date.text(dateHtml);
+  setInterval(getDate, 1000);
+}
+
+$logo.click(getDate);
+
+getDate();
